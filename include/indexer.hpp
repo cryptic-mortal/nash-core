2e9ec1148c570bc100d16553299572f07bcf96e8
Manthan Bagade <178804616+cryptic-mortal@users.noreply.github.com>
2025-12-25 23:46:47 +0530

    used set to remove duplicates items. Used bidirectional mapping to help store the strategy and then later used it in threading

---
+++ include/indexer.hpp
@@ -0,0 +1,70 @@
+#include <unordered_map>
+#include <vector>
+#include <string>
+#include <set>
+#include "game_def.hpp"
+#include "info_set.hpp"
+#include "dealer.hpp"
+#include "rng.hpp"
+#ifndef INDEXER_HPP
+#define INDEXER_HPP
+
+namespace Leduc {
+
+class Indexer {
+    std::unordered_map<std::string,int> key_to_id;
+    std::vector<std::string> id_to_key;
+    std::set<std::string> sorted_key;
+    void walk(Leduc::GameState state) {
+        std::string info = Leduc::get_info_set_key(state);
+        sorted_key.insert(info); 
+        if(state.is_terminal) return;
+        for(int i = 0;i<3;i++) {
+            Leduc::Action act = static_cast<Leduc::Action>(i);
+            if(act==Leduc::Action::BET_RAISE && state.raises_this_round>=4) continue;
+            Leduc::GameState next_state = Leduc::step(state,act);
+            if(state.round==next_state.round) walk(next_state);
+            else {
+                next_state.public_card = Leduc::JACK*2;
+                walk(next_state);
+                next_state.public_card = Leduc::QUEEN*2;
+                walk(next_state);
+                next_state.public_card = Leduc::KING*2;
+                walk(next_state);
+            }
+        }
+    }
+    public:
+    Indexer() {
+        RNG rng(100);
+        Leduc::GameState state = Leduc::deal_initial_state(rng);
+        state.p1_card = Leduc::JACK*2;
+        state.p2_card = Leduc::JACK*2;
+        walk(state);
+        state.p1_card = Leduc::KING*2;
+        state.p2_card = Leduc::KING*2;
+        walk(state);
+        state.p1_card = Leduc::QUEEN*2;
+        state.p2_card = Leduc::QUEEN*2;
+        walk(state);
+        for(auto key : sorted_key) {
+            id_to_key.push_back(key);
+            key_to_id[key] = id_to_key.size()-1;
+        }
+    }
+    int get_id(std::string key) {
+        if(key_to_id.count(key)>0) return key_to_id[key];
+        else return -1;
+    }
+    std::string get_key(int id) {
+        return id_to_key[id];
+    }
+    int get_size() {
+        return id_to_key.size();
+    }
+};
+
+}
+
+
+#endif
